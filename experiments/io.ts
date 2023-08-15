26eeedbec2b958899c957def9a6c26dc1b9cb43b
firesquid <jdeiss06@gmail.com>
2023-08-15 18:41:33 -0500

    remove experiments

--- experiments/io.ts
+++
@@ -1,9 +0,0 @@
-import { io } from "../index.js";
-
-io.prompt<string>("What is your name?")
-  .then((name: string) => {
-    console.log(`Hello ${name}!`);
-  })
-  .catch((e: Error) => {
-    console.error(e);
-  });
