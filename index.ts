9b92f4d703e0e9e44bf8000c0b20b7ca0bc74120
firesquid <jdeiss06@gmail.com>
2023-08-17 18:59:42 -0500

    move kit

--- index.ts
+++ index.ts
@@ -1,8 +1,7 @@
-import { powertool, io } from "./lib/kit.js";
 import {
   getConfig,
   getInstalled,
   ensureApplicationFilesExist,
 } from "./lib/core.js";
 
-export { powertool, io, getConfig, getInstalled, ensureApplicationFilesExist };
+export { getConfig, getInstalled, ensureApplicationFilesExist };
