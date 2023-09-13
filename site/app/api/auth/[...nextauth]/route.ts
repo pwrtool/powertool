5c55d0f57df1720d0f7972036107710c75311519
firesquid <jdeiss06@gmail.com>
2023-09-12 20:17:59 -0500

    add uid to session

--- site/app/api/auth/[...nextauth]/route.ts
+++ site/app/api/auth/[...nextauth]/route.ts
@@ -8,6 +8,25 @@ const handler = NextAuth({
       clientSecret: process.env.GITHUB_SECRET || "",
     }),
   ],
+
+  callbacks: {
+    session: async ({ session, token }) => {
+      if (session?.user) {
+        //@ts-expect-error session.user.id is not in the type but it exists I promise
+        session.user.id = token.uid;
+      }
+      return session;
+    },
+    jwt: async ({ user, token }) => {
+      if (user) {
+        token.uid = user.id;
+      }
+      return token;
+    },
+  },
+  session: {
+    strategy: "jwt",
+  },
 });
 
 export { handler as GET, handler as POST };
--- site/components/SessionUser.tsx
+++ site/components/SessionUser.tsx
@@ -4,11 +4,11 @@ import { useSession } from "next-auth/react";
 
 export default function SessionUser() {
   const { data: session, status } = useSession();
+  console.log(session);
 
   return (
     <div>
-      ClientComponent {status}{" "}
-      {status === "authenticated" && session.user?.email}
+      ClientComponent {status} {status === "authenticated" && session.user?.id}
     </div>
   );
 }
