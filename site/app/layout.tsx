7606e769de9182767fe88422606524efcb7293c8
firesquid <jdeiss06@gmail.com>
2023-09-23 10:57:56 -0500

    add update

--- site/app/layout.tsx
+++ site/app/layout.tsx
@@ -6,6 +6,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { GithubProvider } from "@/components/GithubProvider";
 
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
