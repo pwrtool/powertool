1882b23f78ef55608a06ef674ec2d4e874ef4b12
firesquid <jdeiss06@gmail.com>
2023-10-04 22:00:49 -0500

    make the cli the only project in this repo

--- site/app/docs/page.md
+++
@@ -1,9 +0,0 @@
----
-title: my markdown file
-description: made with markdoc
----
-
-# hello world!
-
-
-this is a markdown file!
--- site/lib/server/db.ts
+++
@@ -1,7 +0,0 @@
-import clientPromise from "./mongodb";
-
-export async function getDb() {
-  const client = await clientPromise;
-
-  return client.db("powertool");
-}
