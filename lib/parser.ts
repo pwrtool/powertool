a62dcedd6c5ea7d500609ad39499d8f6a72d7d76
firesquid <jdeiss06@gmail.com>
2023-10-11 18:27:21 -0500

    add command line parser

--- lib/parser.ts
+++ lib/parser.ts
@@ -44,5 +44,20 @@ export function parseArgs(args: string[]): [ParsedRunstring, string] {
     autoAnswer: false,
   };
 
+  for (let i = 0; i < args.length; i++) {
+    if (args[i].includes("=")) {
+      const [key, value] = args[i].split("=");
+      runstring.arguments.set(key, value);
+    } else if (args[i].includes("/")) {
+      kit = args[i];
+    } else if (isTool(args[i])) {
+      runstring.tool = args[i];
+    }
+  }
+
   return [runstring, kit];
+}
+
+function isTool(word: string): boolean {
+  return !(word.includes(".") || word.includes("/") || word === "ptx");
 }
--- test/parser.test.ts
+++ test/parser.test.ts
@@ -1,7 +1,53 @@
 import { describe, test, expect } from "bun:test";
+import { parseArgs } from "../lib/parser";
 
 describe("parser", () => {
   test("should parse a simple string", () => {
-    expect("hello").toEqual("hello");
+    expect(parseArgs(["ptx", "me/mykit", "mytool", "arg1=val1"])).toEqual([
+      {
+        tool: "mytool",
+        from: process.cwd(),
+        arguments: new Map([["arg1", "val1"]]),
+        answers: [],
+        autoAnswer: false,
+      },
+      "me/mykit",
+    ]);
+  });
+  test("should deal with no tool", () => {
+    expect(parseArgs(["ptx", "me/mykit", "arg1=val1"])).toEqual([
+      {
+        tool: "",
+        from: process.cwd(),
+        arguments: new Map([["arg1", "val1"]]),
+        answers: [],
+        autoAnswer: false,
+      },
+      "me/mykit",
+    ]);
+  });
+  test("should deal with no args", () => {
+    expect(parseArgs(["ptx", "me/mykit", "mytool"])).toEqual([
+      {
+        tool: "mytool",
+        from: process.cwd(),
+        arguments: new Map(),
+        answers: [],
+        autoAnswer: false,
+      },
+      "me/mykit",
+    ]);
+  });
+  test("should deal with no kit", () => {
+    expect(parseArgs(["ptx", "mytool", "arg1=val1"])).toEqual([
+      {
+        tool: "mytool",
+        from: process.cwd(),
+        arguments: new Map([["arg1", "val1"]]),
+        answers: [],
+        autoAnswer: false,
+      },
+      "",
+    ]);
   });
 });
