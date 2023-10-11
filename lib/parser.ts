817235948006218030ae45b1e581711a5b1fa34c
firesquid <jdeiss06@gmail.com>
2023-10-11 18:28:02 -0500

    remove deprecated methods

--- lib/parser.ts
+++ lib/parser.ts
@@ -1,39 +1,5 @@
 import { ParsedRunstring } from "@pwrtool/runstring";
 
-// DEPRECATED
-export function usefulArgs(): string[] {
-  for (let i = 0; i < process.argv.length; i++) {
-    if (process.argv[i].includes("ptx.ts") || process.argv[i] === "ptx") {
-      return process.argv.slice(i + 1);
-    }
-  }
-
-  return [];
-}
-
-// DEPRECATED
-export function getKit(args: string[]): {
-  kit: string;
-  tool: string;
-} {
-  return {
-    kit: args[0],
-    tool: args[1],
-  };
-}
-
-export function getParameters(args: string[]): string[] {
-  const params: string[] = [];
-
-  for (let i = 0; i < args.length; i++) {
-    if (args[i].includes("=")) {
-      params.push(args[i]);
-    }
-  }
-
-  return params;
-}
-
 export function parseArgs(args: string[]): [ParsedRunstring, string] {
   let kit = "";
   const runstring: ParsedRunstring = {
