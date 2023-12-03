f74947bd2bb572acd46ff462849351214bc00821
firesquid <jdeiss06@gmail.com>
2023-12-03 16:54:06 -0600

    add basic commands

--- cli/go.mod
+++ cli/go.mod
@@ -2,15 +2,4 @@ module github.com/pwrtool/powertool/pt
 
 go 1.21.3
 
-require (
-	github.com/fatih/color v1.16.0
-	github.com/urfave/cli v1.22.14
-)
-
-require (
-	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
-	github.com/mattn/go-colorable v0.1.13 // indirect
-	github.com/mattn/go-isatty v0.0.20 // indirect
-	github.com/russross/blackfriday/v2 v2.1.0 // indirect
-	golang.org/x/sys v0.14.0 // indirect
-)
+require github.com/akamensky/argparse v1.4.0 // indirect
--- cli/main.go
+++ cli/main.go
@@ -1,64 +1,16 @@
 package main
 
 import (
-	"github.com/fatih/color"
-	//pt "github.com/pwrtool/powertool/core"
-	"fmt"
-	"github.com/urfave/cli"
-	"log"
-	"os"
+	"github.com/akamensky/argparse"
 )
 
 func main() {
-	//c := color.New(color.FgHiWhite).Add(color.Bold)
+	parser := argparse.NewParser("pt", "A CLI frontend for powertool")
 
-	app := &cli.App{
-		Commands: []*cli.Command{
-			{
-				Name:    "add",
-				Aliases: []string{"a"},
-				Usage:   "add a task to the list",
-				Action: func(cCtx *cli.Context) error {
-					fmt.Println("added task: ", cCtx.Args().First())
-					return nil
-				},
-			},
-			{
-				Name:    "complete",
-				Aliases: []string{"c"},
-				Usage:   "complete a task on the list",
-				Action: func(cCtx *cli.Context) error {
-					fmt.Println("completed task: ", cCtx.Args().First())
-					return nil
-				},
-			},
-			{
-				Name:    "template",
-				Aliases: []string{"t"},
-				Usage:   "options for task templates",
-				Subcommands: []*cli.Command{
-					{
-						Name:  "add",
-						Usage: "add a new template",
-						Action: func(cCtx *cli.Context) error {
-							fmt.Println("new task template: ", cCtx.Args().First())
-							return nil
-						},
-					},
-					{
-						Name:  "remove",
-						Usage: "remove an existing template",
-						Action: func(cCtx *cli.Context) error {
-							fmt.Println("removed task template: ", cCtx.Args().First())
-							return nil
-						},
-					},
-				},
-			},
-		},
-	}
-
-	if err := app.Run(os.Args); err != nil {
-		log.Fatal(err)
-	}
+	parser.NewCommand("x", "Executes a powertool from the specified kit")
+	parser.NewCommand("install", "Downloads and installs a specified kit from a url")
+	parser.NewCommand("remove", "Removes a specified kit")
+	parser.NewCommand("list", "Lists all installed kits")
+	parser.NewCommand("update", "Updates a specified kit")
+	parser.NewCommand("info", "Displays information about a specified kit")
 }
