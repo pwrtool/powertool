e48daaab9d7a746195775900cbae7fc992a10ce1
firesquid <jdeiss06@gmail.com>
2023-12-02 20:15:42 -0600

    add cobra

--- cli/main.go
+++ cli/main.go
@@ -1,7 +1,36 @@
 package main
 
-import "github.com/pwrtool/powertool/cli/cmd"
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"os"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "pt",
+	Short: "Automate everything everywhere",
+	Long:  "The CLI frontend for powertool. See full documentation at https://powertool.dev",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Hello, world!")
+	},
+}
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print the version number of pt",
+	Long:  "Prints the version number of the powertool cli",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("pt v0.0.1")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
 
 func main() {
-	cmd.Execute()
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Print(err)
+		os.Exit(1)
+	}
 }
