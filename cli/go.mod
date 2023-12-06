7ba310ae9f8735de6930530dc41f5751ef9964fd
firesquid <jdeiss06@gmail.com>
2023-12-05 20:00:19 -0600

    use kong

--- cli/go.mod
+++ cli/go.mod
@@ -2,4 +2,4 @@ module github.com/pwrtool/powertool/pt
 
 go 1.21.3
 
-require github.com/akamensky/argparse v1.4.0 // indirect
+require github.com/alecthomas/kong v0.8.1 // indirect
--- cli/main.go
+++ cli/main.go
@@ -1,4 +1,35 @@
 package main
 
+import (
+	"fmt"
+	"github.com/alecthomas/kong"
+)
+
+var CLI struct {
+	X struct {
+		Kit    string `arg:"" name:"kit" help:"Kit to use." required:""`
+		Tool   string `arg:"" name:"tool" help:"Tool to use." required:""`
+		Silent bool   `name:"silent" short:"s" help:"Runs powertool with no extra output" default:"0" optional:""`
+	} `cmd:"" help:"Runs a specified powertool"`
+	Install struct {
+		Kit string `arg:"" help:"Kit to install."`
+	} `cmd:"" help:"Installs a specified kit"`
+}
+
 func main() {
+	ctx := kong.Parse(&CLI)
+	switch ctx.Command() {
+	case "x <kit> <tool>":
+		kit := CLI.X.Kit
+		tool := CLI.X.Tool
+		slient := CLI.X.Silent
+		fmt.Println("Running:", kit, tool)
+		fmt.Println("Silent:", slient)
+	case "install":
+		kit := CLI.Install.Kit
+		fmt.Println("install", kit)
+	default:
+		fmt.Println("Command not found")
+	}
+
 }
