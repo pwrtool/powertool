7c084b90794f2f6234e711bd643dd77c85cb9c55
firesquid <jdeiss06@gmail.com>
2023-12-05 19:27:30 -0600

    remove argparse

--- cli/main.go
+++ cli/main.go
@@ -1,16 +1,4 @@
 package main
 
-import (
-	"github.com/akamensky/argparse"
-)
-
 func main() {
-	parser := argparse.NewParser("pt", "A CLI frontend for powertool")
-
-	parser.NewCommand("x", "Executes a powertool from the specified kit")
-	parser.NewCommand("install", "Downloads and installs a specified kit from a url")
-	parser.NewCommand("remove", "Removes a specified kit")
-	parser.NewCommand("list", "Lists all installed kits")
-	parser.NewCommand("update", "Updates a specified kit")
-	parser.NewCommand("info", "Displays information about a specified kit")
 }
