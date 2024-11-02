f97a0b18db9babbff58068258e3b3867dca2d1ea
firesquid6 <jdeiss06@gmail.com>
2024-11-02 11:02:41 -0500

    add flake.nixx

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+  outputs = { self, nixpkgs }: 
+  let 
+    pkgs = nixpkgs.legacyPackages.x86_64-linux; 
+  in { 
+    devShells.x86_64-linux.default = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        nodejs_20
+        go
+        gopls
+      ];
+    };
+  };
+}
