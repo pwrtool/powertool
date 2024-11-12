f38dc791ff05e3010fe297947e4b40150211dff1
firesquid6 <jdeiss06@gmail.com>
2024-11-12 17:26:28 -0600

    add tests for text splitting

--- runes/runes.go
+++ runes/runes.go
@@ -1,108 +1,100 @@
 package runes
 
 func Split(text []rune, separator rune) [][]rune {
-  sections := [][]rune{}
-  section := []rune{}
+	sections := [][]rune{}
+	section := []rune{}
 
-  for _, c := range text {
-    if c == separator {
-      sections = append(sections, section)
-      section = []rune{}
-    } else {
-      section = append(section, c)
-    }
-  }
+	for _, c := range text {
+		if c == separator {
+			sections = append(sections, section)
+			section = []rune{}
+		} else {
+			section = append(section, c)
+		}
+	}
 
-  sections = append(sections, section)
-  return sections
+	sections = append(sections, section)
+	return sections
 }
-
 
 func Equal(text []rune, other []rune) bool {
-  if len(text) != len(other) {
-    return false
-  }
+	if len(text) != len(other) {
+		return false
+	}
 
-  for i := range text {
-    if text[i] != other[i] {
-      return false
-    }
-  }
+	for i := range text {
+		if text[i] != other[i] {
+			return false
+		}
+	}
 
-  return true
+	return true
 }
-
 
 func ExtractInside(text []rune, delimeter rune) [][]rune {
-  parts := [][]rune{}
-  current := []rune{}
-  var inside bool = false
+	parts := [][]rune{}
+	current := []rune{}
+	var inside bool = false
 
-  for _, c := range text {
-    if inside {
-      if c == delimeter {
-        inside = false
-        parts = append(parts, current)
-        current = []rune{}
-      } else {
-        current  = append(current, c)
-      }
-    } else {
-      if c == delimeter {
-        inside = true
-      }
-    }
-  }
-  
-  return parts
+	for _, c := range text {
+		if inside {
+			if c == delimeter {
+				inside = false
+				parts = append(parts, current)
+				current = []rune{}
+			} else {
+				current = append(current, c)
+			}
+		} else {
+			if c == delimeter {
+				inside = true
+			}
+		}
+	}
+
+	return parts
 }
-
 
 func RemoveWhitespace(text []rune) []rune {
-  parts := []rune{}
-  for _, c := range text {
-    if c != ' ' && c != '\t' && c != '\n' {
-      parts = append(parts, c)
-    }
-  }
-  return parts
+	parts := []rune{}
+	for _, c := range text {
+		if c != ' ' && c != '\t' && c != '\n' {
+			parts = append(parts, c)
+		}
+	}
+	return parts
 }
-
-
 
 func TrimLeft(text []rune) []rune {
-  i := 0
+	i := 0
 
-  for i < len(text) {
-    if !(i == ' ' || i == '\t' || i == '\n') {
-      break
-    }
+	for i < len(text) {
+		if !(i == ' ' || i == '\t' || i == '\n') {
+			break
+		}
 
-    i += 1
-  }
+		i += 1
+	}
 
-  // TODO - do I need to copy?
-  return text[i:]
+	// TODO - do I need to copy?
+	return text[i:]
 }
-
 
 func TrimRight(text []rune) []rune {
-  i := len(text) - 1
+	i := len(text) - 1
 
-  for i > 0 {
-    if !(i == ' ' || i == '\t' || i == '\n') {
-      break
-    }
+	for i > 0 {
+		if !(i == ' ' || i == '\t' || i == '\n') {
+			break
+		}
 
-    i -= 1
-  }
+		i -= 1
+	}
 
-
-  return text[0:i + 1]
+	return text[0 : i+1]
 }
 
-
 func TrimAround(text []rune) []rune {
-  text = TrimRight(text)
-  return TrimLeft(text)
+	text = TrimRight(text)
+	return TrimLeft(text)
 }
--- runes/runes_test.go
+++ runes/runes_test.go
@@ -5,62 +5,85 @@ import (
 	"testing"
 )
 
-
 func TestSplit(t *testing.T) {
-  cases := []struct{
-    input []rune
-    separator rune
-    expectation [][]rune
-  }{
+	cases := []struct {
+		input       []rune
+		separator   rune
+		expectation [][]rune
+	}{
+		{
+			input:     []rune("a = b"),
+			separator: '=',
+			expectation: [][]rune{
+				[]rune("a "),
+				[]rune(" b"),
+			},
+		},
+		{
+			input:     []rune("==="),
+			separator: '=',
+			expectation: [][]rune{
+				{},
+				{},
+				{},
+				{},
+			},
+		},
+	}
+
+	for _, c := range cases {
+		result := Split(c.input, c.separator)
+
+		if len(result) != len(c.expectation) {
+			printSplitFailure(result, c.expectation)
+			t.Fail()
+		}
+
+		for i := range result {
+			if !Equal(result[i], c.expectation[i]) {
+				printSplitFailure(result, c.expectation)
+				t.Fail()
+			}
+		}
+	}
+}
+
+func TestTrim(t *testing.T) {
+	cases := []struct {
+		input       []rune
+		leftExpectation []rune
+    rightExpectation []rune
+	}{
     {
-      input: []rune("a = b"),
-      separator: '=',
-      expectation: [][]rune{
-        []rune("a "),
-        []rune(" b"),
-      },
-    },
-    {
-      input: []rune("==="),
-      separator: '=',
-      expectation: [][]rune{
-        {},
-        {},
-        {},
-        {},
-      },
+      input: []rune("   look at me!   "),
+      leftExpectation: []rune("look at me!   "),
+      rightExpectation: []rune("   look at me!"),
     },
   }
 
-
   for _, c := range cases {
-    result := Split(c.input, c.separator)
+    lRes := TrimLeft(c.input)
+    rRes := TrimRight(c.input)
 
-    if len(result) != len(c.expectation) {
-      printFailure(result, c.expectation)
+    if !Equal(lRes, c.leftExpectation) {
+
       t.Fail()
     }
 
-    for i := range result {
-      if !Equal(result[i], c.expectation[i]) {
-        printFailure(result, c.expectation)
-        t.Fail()
-      }
+    if !Equal(rRes, c.rightExpectation) {
+      t.Fail()
     }
   }
 }
 
-
-
-
-func printFailure(output [][]rune, expectation [][]rune) {
-  fmt.Println("FAILED!!!")
-  fmt.Println("Expected:")
-  for _, part := range expectation {
-    fmt.Println("|", part, "|")
-  }
-  fmt.Println("\nGot:")
-  for _, part := range output {
-    fmt.Println("|", part, "|")
-  }
+func printSplitFailure(output [][]rune, expectation [][]rune) {
+	fmt.Println("FAILED!!!")
+	fmt.Println("Expected:")
+	for _, part := range expectation {
+		fmt.Println("|", part, "|")
+	}
+	fmt.Println("\nGot:")
+	for _, part := range output {
+		fmt.Println("|", part, "|")
+	}
 }
