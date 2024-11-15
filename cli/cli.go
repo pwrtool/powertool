362f096d0967599ac62d6fed206c470980287d4e
firesquid6 <jdeiss06@gmail.com>
2024-11-14 22:47:26 -0600

    reformat modules

--- cli/cli.go
+++ cli/cli.go
@@ -2,108 +2,103 @@ package cli
 
 import (
 	"errors"
-	"strings"
-
 	"github.com/pwrtool/powertool/parser"
+	"strconv"
+	"strings"
 )
 
 const STRING_FALSE = "FALSE"
 const STRING_TRUE = "TRUE"
 
 func ParseCommandArgs(args []string, options []parser.Option) (map[string]string, error) {
-  argMap := map[string]string{}
-  unsatisfiedOptions := []string{}
+	argMap := map[string]string{}
+	unsatisfiedOptions := []string{}
 
-  for _, option := range options {
-    if option.IsBoolean {
-      argMap[option.Name] = STRING_FALSE
-    } else if option.DefaultValue != "" {
-      argMap[option.Name] = option.DefaultValue;
-    } else {
-      // option is required!
-      unsatisfiedOptions = append(unsatisfiedOptions, option.Name)
-    }
-  }
+	for _, option := range options {
+		if option.IsBoolean {
+			argMap[option.Name] = STRING_FALSE
+		} else if option.DefaultValue != "" {
+			argMap[option.Name] = option.DefaultValue
+		} else {
+			// option is required!
+			unsatisfiedOptions = append(unsatisfiedOptions, option.Name)
+		}
+	}
 
-  var i = 0;
-  var positional = 1;
+	var i = 0
+	var positional = 1
 
-  for i < len(args) {
-    if !(isFlag(args[i])) {
-      // it's a positional arg
-      option, err := getOptionWithPosition(positional, options)
+	for i < len(args) {
+		if !(isFlag(args[i])) {
+			// it's a positional arg
+			option, err := getOptionWithPosition(positional, options)
 
-      if err != nil {
-        return argMap, err
-      }
+			if err != nil {
+				return argMap, err
+			}
 
-      argMap[option.Name] = args[i]
+			argMap[option.Name] = args[i]
 
-      positional += 1
-      continue
-    }
+			positional += 1
+			continue
+		}
 
-    option, err := getOptionWithArg(args[i], options)
-    if err != nil {
-      return argMap, err
-    }
+		option, err := getOptionWithArg(args[i], options)
+		if err != nil {
+			return argMap, err
+		}
 
-    if option.IsBoolean {
-      argMap[option.Name] = STRING_TRUE
-    } else {
-      if i + 1 >= len(args) {
-        return argMap, errors.New("No value for flag " + args[i])
-      }
-      i += 1
+		if option.IsBoolean {
+			argMap[option.Name] = STRING_TRUE
+		} else {
+			if i+1 >= len(args) {
+				return argMap, errors.New("No value for flag " + args[i])
+			}
+			i += 1
 
-      value := args[i]
-      
-      if isFlag(value) {
-        return argMap, errors.New("Expected value, got flag " + value)
-      }
+			value := args[i]
 
-      argMap[option.Name] = value
-    }
-    
-    i += 1
-  }
+			if isFlag(value) {
+				return argMap, errors.New("Expected value, got flag " + value)
+			}
 
+			argMap[option.Name] = value
+		}
 
+		i += 1
+	}
 
-  if len(unsatisfiedOptions) > 0 {
-    return argMap, errors.New("Option " + unsatisfiedOptions[0] + " was not satisfied")
+	if len(unsatisfiedOptions) > 0 {
+		return argMap, errors.New("Option " + unsatisfiedOptions[0] + " was not satisfied")
 
-  }
+	}
 
-
-  return argMap, nil
+	return argMap, nil
 
 }
 
-
 func getOptionWithArg(arg string, options []parser.Option) (parser.Option, error) {
-  for _, option := range options {
-    for _, flag := range option.PossibleFlags {
-      if flag == arg {
-        return option, nil
-      }
-    }
-  }
+	for _, option := range options {
+		for _, flag := range option.PossibleFlags {
+			if flag == arg {
+				return option, nil
+			}
+		}
+	}
 
-
-  return parser.Option{}, errors.New("No option with argument " + arg)
+	return parser.Option{}, errors.New("No option with argument " + arg)
 }
 
 func getOptionWithPosition(position int, options []parser.Option) (parser.Option, error) {
-  for _, option := range options {
-    if option.Position == position {
-      return option, nil
-    }
-  }
+	for _, option := range options {
+		if option.Position == position {
+			return option, nil
+		}
+	}
 
-  return parser.Option{}, errors.New("no option for position " + string(position) + " found")
+	return parser.Option{}, errors.New("no option for position " + strconv.Itoa(position) + " found")
 }
 
 func isFlag(s string) bool {
-  return (strings.HasPrefix(s, "-") || strings.HasPrefix(s, "--"))
+	return (strings.HasPrefix(s, "-") || strings.HasPrefix(s, "--"))
 }
--- cli/cli_test.go
+++ cli/cli_test.go
@@ -2,9 +2,9 @@ package cli_test
 
 import (
 	"fmt"
-  "reflect"
+	. "github.com/pwrtool/powertool/cli"
 	"github.com/pwrtool/powertool/parser"
-  . "github.com/pwrtool/powertool/cli"
+	"reflect"
 	"testing"
 )
 
@@ -21,57 +21,57 @@ func TestParseCommandArgs(t *testing.T) {
 				"--boolean",
 				"positionalValue",
 			},
-      options: []parser.Option{
-        {
-          Name: "config",
-          PossibleFlags: []string{"-c"},
-          DefaultValue: "defaultConfig",
-          IsBoolean: false,
-          Position: -1,
-        },
-        {
-          Name: "positional",
-          PossibleFlags: []string{},
-          DefaultValue: "",
-          IsBoolean: false,
-          Position: 1,
-        },
-        {
-          Name: "boolean",
-          PossibleFlags: []string{"--boolean"},
-          DefaultValue: "",
-          IsBoolean: true,
-          Position: -1,
-        },
-      },
-      expected: map[string]string{
-        "config": "myCustomConfig",
-        "boolean": STRING_TRUE,
-        "positional": "positionalValue",
-      },
+			options: []parser.Option{
+				{
+					Name:          "config",
+					PossibleFlags: []string{"-c"},
+					DefaultValue:  "defaultConfig",
+					IsBoolean:     false,
+					Position:      -1,
+				},
+				{
+					Name:          "positional",
+					PossibleFlags: []string{},
+					DefaultValue:  "",
+					IsBoolean:     false,
+					Position:      1,
+				},
+				{
+					Name:          "boolean",
+					PossibleFlags: []string{"--boolean"},
+					DefaultValue:  "",
+					IsBoolean:     true,
+					Position:      -1,
+				},
+			},
+			expected: map[string]string{
+				"config":     "myCustomConfig",
+				"boolean":    STRING_TRUE,
+				"positional": "positionalValue",
+			},
 		},
 	}
 
-  for _, c := range cases {
-    result, err := ParseCommandArgs(c.args, c.options)
+	for _, c := range cases {
+		result, err := ParseCommandArgs(c.args, c.options)
 
-    if err != nil {
-      fmt.Println("Got error: ")
-      fmt.Println(err)
-      t.Fail()
-    }
+		if err != nil {
+			fmt.Println("Got error: ")
+			fmt.Println(err)
+			t.Fail()
+		}
 
-    if !reflect.DeepEqual(result, c.expected) {
-      fmt.Println("Test failed - bad result")
-      fmt.Println("Expected:")
-      fmt.Printf("%#v\n", c.expected)
+		if !reflect.DeepEqual(result, c.expected) {
+			fmt.Println("Test failed - bad result")
+			fmt.Println("Expected:")
+			fmt.Printf("%#v\n", c.expected)
 
-      fmt.Println("Got:")
-      fmt.Printf("%#v\n", result)
-      
-      t.Fail()
-    }
+			fmt.Println("Got:")
+			fmt.Printf("%#v\n", result)
 
-  }
+			t.Fail()
+		}
+
+	}
 
 }
