9c4509a697287c86125b35e6802196a55a0e5f59
firesquid6 <jdeiss06@gmail.com>
2024-11-14 22:46:15 -0600

    add cli function

--- cli/cli.go
+++ cli/cli.go
@@ -2,6 +2,8 @@ package cli
 
 import (
 	"errors"
+	"strings"
+
 	"github.com/pwrtool/powertool/parser"
 )
 
@@ -23,6 +25,49 @@ func ParseCommandArgs(args []string, options []parser.Option) (map[string]string
     }
   }
 
+  var i = 0;
+  var positional = 1;
+
+  for i < len(args) {
+    if !(isFlag(args[i])) {
+      // it's a positional arg
+      option, err := getOptionWithPosition(positional, options)
+
+      if err != nil {
+        return argMap, err
+      }
+
+      argMap[option.Name] = args[i]
+
+      positional += 1
+      continue
+    }
+
+    option, err := getOptionWithArg(args[i], options)
+    if err != nil {
+      return argMap, err
+    }
+
+    if option.IsBoolean {
+      argMap[option.Name] = STRING_TRUE
+    } else {
+      if i + 1 >= len(args) {
+        return argMap, errors.New("No value for flag " + args[i])
+      }
+      i += 1
+
+      value := args[i]
+      
+      if isFlag(value) {
+        return argMap, errors.New("Expected value, got flag " + value)
+      }
+
+      argMap[option.Name] = value
+    }
+    
+    i += 1
+  }
+
 
 
   if len(unsatisfiedOptions) > 0 {
@@ -33,4 +78,32 @@ func ParseCommandArgs(args []string, options []parser.Option) (map[string]string
 
   return argMap, nil
 
+}
+
+
+func getOptionWithArg(arg string, options []parser.Option) (parser.Option, error) {
+  for _, option := range options {
+    for _, flag := range option.PossibleFlags {
+      if flag == arg {
+        return option, nil
+      }
+    }
+  }
+
+
+  return parser.Option{}, errors.New("No option with argument " + arg)
+}
+
+func getOptionWithPosition(position int, options []parser.Option) (parser.Option, error) {
+  for _, option := range options {
+    if option.Position == position {
+      return option, nil
+    }
+  }
+
+  return parser.Option{}, errors.New("no option for position " + string(position) + " found")
+}
+
+func isFlag(s string) bool {
+  return (strings.HasPrefix(s, "-") || strings.HasPrefix(s, "--"))
 }
